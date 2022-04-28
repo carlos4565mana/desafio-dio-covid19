efd0622504162d71a0dc30ab11c67267da890add
carlos4565mana <carloscal61@gmail.com>
2022-04-28 15:43:19 -0300

    implementing functions in the index.js api

--- desafio-dio-covid/src/App.jsx
+++ desafio-dio-covid/src/App.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 
+import{ fetchData, fetchDailyData, fetchCountries} from './api'
+
 class App extends React.Component{
+  async componentDidMount(){
+    const data = await fetchCountries();
+    console.log(data);
+  }
   render(){
     return(
       <div>
